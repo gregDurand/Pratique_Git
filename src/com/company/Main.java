19fa062b71a4fcd455bf538b826b4a3c10e3edaa
gregory.durand <gregory.durand@he-arc.ch>
2018-10-14 15:45:09 +0200

    update main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import com.company.OtherPackage.Branche_4;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Branche_1 b1 = new Branche_1();
+        Branche_4 b4 = new Branche_4();
+        Branche_2 b2 = new Branche_2();
     }
 }
